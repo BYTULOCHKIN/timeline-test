b7eb44a93d293867177fbec52298e6755be00be1
Ruslan Krasiuk <rus.krasuk@gmail.com>
2026-05-29 11:51:02 +0300

    fix: deleted check env logic

--- src/main.tsx
+++ src/main.tsx
@@ -48,7 +48,6 @@ const svgUseConfig: Config = {
     runtimeChecksEnabled: import.meta.env.DEV,
 };
 
-checkEnv(envSchema);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <SvgUseConfigContext.Provider value={svgUseConfig}>
